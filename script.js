46b8bb259c86f2a85a5ed6ecf712d3f74ee302ad
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-06-01 19:07:12 -0300

    exercicio 8

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-window.onload = function() {
-    let corInicial = document.getElementById('black').className += ' selected';
-    let cores = document.querySelector('#color-palette');
-    cores.addEventListener('click', trocarSelected);    
-  function trocarSelected(event) {
-    if (event.target.className === 'color') {      
-      let atual = document.querySelector('.selected').classList.remove('selected');   
-      event.target.className += ' selected';
+window.onload = function inicio() {
+  document.getElementById('black').classList.add('selected');
+  function criarCores() {
+    document.getElementById('black').style.backgroundColor = 'black';
+    document.getElementById('red').style.backgroundColor = 'red';
+    document.getElementById('blue').style.backgroundColor = 'blue';
+    document.getElementById('green').style.backgroundColor = 'green';
+  }
+  criarCores();  
+};
+  function trocarSelected(clique) {
+  if (clique.target.className === 'color') {    
+    document.querySelector('.selected').classList.remove('selected');
+    clique.target.className += ' selected';    
+  }
+}
+  document.querySelector('#color-palette').addEventListener('click', trocarSelected);
+  function mudarCor(clique) {
+    let corSelected = document.querySelector('.selected').style.backgroundColor;
+    if (clique.target.className === 'pixel') {
+      clique.target.style.backgroundColor = corSelected;
     }
   }
-
-
-
-
-}
+  document.querySelector('#pixel-board').addEventListener('click', mudarCor);
